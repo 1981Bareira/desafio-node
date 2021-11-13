f5f0712a888bdbcb3beefb28ebda9641b210fd78
1981Bareira <renatotom@gmail.com>
2021-11-13 12:16:39 -0300

    commit initial

---
+++ init.sql
@@ -0,0 +1,5 @@
+use nodedb;
+CREATE TABLE IF NOT EXISTS people(
+ID int not null auto_increment,
+name VARCHAR(255),
+primary key(id));
--- nodejs/index.js
+++ nodejs/index.js
@@ -1,11 +1,38 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database:'nodedb'
+};
+const mysql = require('mysql2')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) values('Wesley')`
+
+
+
+
 
 
 
 app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle</h1>')
+    connection.query(sql);
+    connection.query(`SELECT * from people`,function (err,results){
+        let objeto = '<h1>Full Cycle</h1> <br> <br>';
+        for(i=0;i< results.length; i++){
+            objeto += results[i].name + '<br>';
+            
+            
+        }
+    
+    res.send(objeto);
+        
+    } );
+
+   
 })
 
 app.listen(port, ()=> {
